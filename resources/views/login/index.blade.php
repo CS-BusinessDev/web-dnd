2f503a9d18fc25a1e5d2cf2efd5582f2213df30d
apriansyahrs <apriansyahrizs@gmail.com>
2024-10-28 13:16:54 +0700

    fix: add pwa in login page

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -15,6 +15,11 @@
     <link rel="stylesheet" href="{{ asset('template') }}/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
     <!-- Theme style -->
     <link rel="stylesheet" href="{{ asset('template') }}/dist/css/adminlte.min.css">
+
+    <!-- PWA  -->
+    <meta name="theme-color" content="#6777ef" />
+    <link rel="apple-touch-icon" href="{{ asset('logo.png') }}">
+    <link rel="manifest" href="{{ asset('manifest.json') }}">
 </head>
 
 <body class="hold-transition login-page">
@@ -47,7 +52,8 @@
                     <div class="row justify-content-center">
                         <!-- /.col -->
                         <div class="col-4 ">
-                            <button type="submit" class="btn btn-block" style="background-color: #917FB3; color: white;">Sign In</button>
+                            <button type="submit" class="btn btn-block"
+                                style="background-color: #917FB3; color: white;">Sign In</button>
                         </div>
                         <!-- /.col -->
                     </div>
@@ -58,6 +64,24 @@
         <!-- /.card -->
     </div>
     <!-- /.login-box -->
+
+    <script src="{{ asset('sw.js') }}"></script>
+    <script>
+        if ("serviceWorker" in navigator) {
+            // Register a service worker hosted at the root of the
+            // site using the default scope.
+            navigator.serviceWorker.register("sw.js").then(
+                (registration) => {
+                    console.log("Service worker registration succeeded:", registration);
+                },
+                (error) => {
+                    console.error(`Service worker registration failed: ${error}`);
+                },
+            );
+        } else {
+            console.error("Service workers are not supported.");
+        }
+    </script>
 
 </body>
 
